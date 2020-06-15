ea37bc351461b29899cc03432f54b65669b83c75
Juho Hautala <juho.hautala@vincit.com>
2020-06-14 23:11:51 -0700

    client: Use style from props

--- client/src/components/common/Spinner.jsx
+++ client/src/components/common/Spinner.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Spinner = () => {
+const Spinner = (props) => {
   // By Sam Herbert (@sherb), for everyone. More @ http://goo.gl/7AJzbL
   return (
-    <div style={style}>
+    <div style={{...style, ...props.style}}>
       <svg viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg" stroke="#000">
         <g fill="none" fillRule="evenodd" strokeWidth="2">
           <circle cx="22" cy="22" r="1">
